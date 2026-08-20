7a7fa6e462d3dce548a684d636ab1a33bfa868b9
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 10:44:10 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
-import PackageDescription
 import CompilerPluginSupport
+import PackageDescription
 
 extension String {
     static let copyOnWrite: Self = "Copy on Write"
@@ -17,20 +17,20 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-copy-on-write",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
             name: .copyOnWrite,
             targets: [.copyOnWrite]
-        ),
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", "602.0.0"..<"603.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", "602.0.0"..<"603.0.0")
     ],
     targets: [
         .target(
